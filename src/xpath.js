b2621cd584f6f5a6b8a54abb41dc2df2e45ee373
ch4rlieb <karel.bel@dativery.com>
2019-04-15 11:49:33 +0200

    tokenize

--- src/xpath.js
+++ src/xpath.js
@@ -1412,11 +1412,11 @@ let xpathfunctions = {
         const str = this.args[0].evaluate(ctx).stringValue();
         const separator = this.args[1].evaluate(ctx).stringValue();
         var ret = str.split(separator);
-        console.log('1 RET FROM TOKENIZE', ret);
+        console.log('2 RET FROM TOKENIZE', ret);
         ret = ret.map((item) => {
             return new StringValue(item);
         });
-        return ret; //new NodeSetValue(ret);
+        return new NodeSetValue(ret);
     }
 };
 
